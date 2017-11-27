59077750610aa584cbdebabd49297279b5b512fb
36115152016.3 <36115152016.3@36DE194547.senacrjedu.local>
2017-11-27 12:05:15 -0200

    aula 5 Lista e vetores

---
+++ src/exemplos/Listas.java
@@ -0,0 +1,48 @@
+package exemplos;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import entidades.Produto;
+
+public class Listas {
+	public static void main(String[] args) {
+		
+		//Primeiro contato com Collections
+		
+		/* Declaração do objeto */
+		Produto pr1 = new Produto();
+		Produto pr2 = new Produto();
+		Produto pr3 = new Produto();
+		
+		
+		
+		/* Declarando uma lista */
+		List<Produto> listaCompras = new ArrayList<Produto>();
+		
+		pr1.setId(10L);
+		pr1.setNmProduto("Arroz");
+		pr1.setPreco(10.5);
+		pr1.setQuantidade(5.);
+		
+		pr2.setId(20L);
+		pr2.setNmProduto("Feijão");
+		pr2.setPreco(7.8);
+		pr2.setQuantidade(2.5);
+		
+		pr3.setId(30L);
+		pr3.setNmProduto("Açucar");
+		pr3.setPreco(30.);
+		pr3.setQuantidade(1.);
+		
+		 /* Atribuindo a lista*/
+		listaCompras.add(pr1);
+		listaCompras.add(pr2);
+		listaCompras.add(pr3);
+		
+		/*saída da lista*/
+		for (Produto produto : listaCompras) {
+			System.out.println(produto);
+		}
+	}
+}
---
+++ src/exemplos/Vetores.java
@@ -0,0 +1,50 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Vetores {
+	public static void main(String[] args) {
+		/* Declaração de variaveis*/
+		int x = 27;
+		Scanner scan= new Scanner(System.in);
+		
+		/* Declarando os vetores */
+		
+		// Definindo vetor vazio
+		int vet_numeros[] = new int[5];
+		
+		// Definindo vetor com valores
+		String vet_palavras[] = {"Java", "Senac", "Rio"};
+		boolean[] vet_booleano = {true, true, true, false};
+		
+		/* Impressão de valores */
+		
+		// for
+		for(int i = 0; i< vet_palavras.length; i++) {
+			System.out.println(vet_palavras[i]);
+		}
+		//foreach
+		for(boolean b: vet_booleano ) {
+			System.out.println(b);
+		}
+		
+		/*Atribuindo valores*/
+		vet_numeros[0] = 30;
+		vet_numeros[1] = x;
+		vet_numeros[2] = scan.nextInt();
+		
+		
+		for(int n : vet_numeros) {
+			System.out.println(n);
+		}
+		
+		int i = 0; 
+		while(i< vet_numeros.length) {
+			System.out.print("Informe o " + (i+1) + "º: ");
+			vet_numeros[i] = scan.nextInt();
+			i++;
+		}
+		
+		scan.close();
+	}
+}
